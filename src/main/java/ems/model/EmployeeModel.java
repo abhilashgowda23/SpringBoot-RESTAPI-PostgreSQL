611701b7d1736864c20cd13e13dc67876c2cc659
Abhilash Gowda <abhilashgowda278@gmail.com>
2024-07-21 16:43:54 +0530

    Create EmployeeModel.java

---
+++ src/main/java/ems/model/EmployeeModel.java
@@ -0,0 +1,19 @@
+package ems.model;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class EmployeeModel {
+
+
+    private Long id;
+
+    private String firstName;
+    private String lastName;
+    private String email_id;
+}
