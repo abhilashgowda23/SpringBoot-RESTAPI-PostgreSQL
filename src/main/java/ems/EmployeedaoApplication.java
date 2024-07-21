b2484df74f2fb57a524c4efa572cad7e16b472c3
Abhilash Gowda <abhilashgowda278@gmail.com>
2024-07-21 16:45:40 +0530

    Create EmployeedaoApplication.java

---
+++ src/main/java/ems/EmployeedaoApplication.java
@@ -0,0 +1,13 @@
+package ems;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeedaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeedaoApplication.class, args);
+	}
+
+}
