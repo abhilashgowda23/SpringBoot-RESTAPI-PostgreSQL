c4fe51be52cf27966fda7efe994754566826c5b4
Abhilash Gowda <abhilashgowda278@gmail.com>
2024-07-21 16:43:27 +0530

    Create ResourceNotFoundException.java

---
+++ src/main/java/ems/exception/ResourceNotFoundException.java
@@ -0,0 +1,14 @@
+package ems.exception;
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class ResourceNotFoundException extends Exception{
+ private static final Long serialVersionUID = 1L;
+    public ResourceNotFoundException(String message){
+        super(message);
+    }
+
+}
