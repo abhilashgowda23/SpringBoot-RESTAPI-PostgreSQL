5467696460be06bcbc197c685a95c0aab0a785a4
Abhilash Gowda <abhilashgowda278@gmail.com>
2024-07-21 16:44:22 +0530

    Create EmployeeRepository.java

---
+++ src/main/java/ems/repository/EmployeeRepository.java
@@ -0,0 +1,10 @@
+package ems.repository;
+
+
+import ems.entity.EmployeeEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<EmployeeEntity,Long> {
+}
